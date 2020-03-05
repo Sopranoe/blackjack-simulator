3d9382e91562e7e06683a2685bddfb103eb270fd
Sopranoe <s0544642@htw-berlin.de>
2020-03-06 00:53:22 +0100

    added split conditions

--- blackjack/round.py
+++ blackjack/round.py
@@ -46,9 +46,17 @@ class Round():
                     elif player.balance < 2*hand.bet:
                         print("Not enough money to double down.")
                 elif action == 'x':  # split
-                    player.split(hand_number)
-                    hand.draw(self.game_deck.game_deck)
-
+                    if (len(hand.cards) == 2 and player.balance >= 2*hand.bet and hand.cards[0][0] == hand.cards[1][0]):
+                        player.split(hand_number)
+                        hand.draw(self.game_deck.game_deck)
+                        print(f'{player.name} {hand.status} after double on hand'
+                              f'({hand_number + 1}) with {"-".join(hand.cards)} - '
+                              f'{str(hand.points)} points')
+                    elif player.balance < 2*hand.bet:
+                        print("Not enough money to split.")
+                    elif player.balance < 2*hand.bet:
+                        print("Splitting not possible.")
+                    
         if all(hand.status == 'bust' for hand in player.hands):
             self.players_alive -= 1
 
