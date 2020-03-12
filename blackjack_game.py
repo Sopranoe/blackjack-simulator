4dbf5660aae14410952a4ae2f24a2e25b022c460
Sopranoe <s0544642@htw-berlin.de>
2020-03-12 23:06:21 +0100

    added seaborn visualization

--- blackjack_game.py
+++ blackjack_game.py
@@ -1,4 +1,8 @@
 import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from blackjack.deck import Deck
 from blackjack.player import Human, BotStatic
 from blackjack.round import Round
@@ -25,24 +29,35 @@ def human_player_game():
 
 
 def simulate_game():
-    players = [BotStatic(500, 15), BotStatic(500, 16),
-               BotStatic(500, 17), BotStatic(500, 18)]
+    players = [BotStatic(100000, 15), BotStatic(100000, 16),
+               BotStatic(100000, 17), BotStatic(100000, 18)]
     deck = Deck()
     run_game = True
 
     while run_game:
         round = Round(players, deck)
         round.play_round()
+        game_history = {}
         if any(player.balance <= 0 for player in players):
             print("first player is out of money")
             print("____________________________")
             for player in players:
+                game_history[player.name] = player.history
                 print(player.name + ":")
                 print(player.history)
                 print("____________________________")
+            visualize_results(game_history)
             run_game = False
         if len(deck.game_deck) < 0.5*deck.card_count:
             deck.reshuffle()
+
+
+def visualize_results(results):
+    sns.set(style="darkgrid")
+    df = pd.DataFrame.from_dict(results)
+    g = sns.relplot(kind="line", data=df)
+    g.fig.autofmt_xdate()
+    plt.show()
 
 
 start_time = time.time()
